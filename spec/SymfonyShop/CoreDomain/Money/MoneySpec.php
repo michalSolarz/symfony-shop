ba99047a2e89b64ad68c5601f38015e786efad25
michalSolarz <michalsolarz83@gmail.com>
2016-06-29 00:11:07 +0200

    Fixed phpspec errors.

--- spec/SymfonyShop/CoreDomain/Money/MoneySpec.php
+++ spec/SymfonyShop/CoreDomain/Money/MoneySpec.php
@@ -7,9 +7,9 @@ use Prophecy\Argument;
 
 class MoneySpec extends ObjectBehavior
 {
-    function it_should_be_created_with_currency_and_amount($amount, $currency)
+    function it_should_be_created_with_currency_and_amount()
     {
-        $this->beConstructedWith($amount, $currency);
+        $this->beConstructedWith(19.99, 'PLN');
         $this->shouldHaveType('SymfonyShop\CoreDomain\Money\Money');
     }
 }
